56833976c3eb152a1aef173b29c756df0d0c5f29
Konrad <konradk95@gmail.com>
2025-02-22 22:49:05 +0100

    init

---
+++ src/main/java/com/ekdev/InvoiceGen/InvoiceGenApplication.java
@@ -0,0 +1,13 @@
+package com.ekdev.InvoiceGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekdev/InvoiceGen/InvoiceGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekdev.InvoiceGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
